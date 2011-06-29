9176f488fecaa9001ea2f4d022b7443e7463951f
Helmy Giacoman <helmygb@gmail.com>
2011-06-29 11:09:21 -0600

    fix

--- utils.py
+++ utils.py
@@ -3,8 +3,7 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
 def save_as_xls(request):
-    tabla = request.POST['tabla']
-    print tabla
+    tabla = request.POST['tabla']    
     response = render_to_response('xls.html', {'tabla': tabla, })
     response['Content-Disposition'] = 'attachment; filename=tabla.xls'
     response['Content-Type'] = 'application/vnd.ms-excel'
