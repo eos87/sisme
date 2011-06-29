dfcac89fa743563accd60b6304f02fdef943d965
Helmy Giacoman <helmygb@gmail.com>
2011-06-29 17:27:42 -0600

    plantillas lista

--- contraparte/forms.py
+++ contraparte/forms.py
@@ -2,7 +2,9 @@
 from django import forms
 from django.forms import ModelForm
 from sisme.fed.models import *
+from sisme.fed.forms import ANIOS
 import datetime
+
 
 MONTH_CHOICES = ((1, 'Enero'), (2, 'Febrero'),
                  (3, 'Marzo'), (4, 'Abril'),
@@ -12,9 +14,10 @@ MONTH_CHOICES = ((1, 'Enero'), (2, 'Febrero'),
                  (11, 'Noviembre'), (12, 'Diciembre'))
 
 class InfluenciaForm(forms.Form):
-    tipo = forms.MultipleChoiceField(choices=MODALIDAD_CHOICE, label='Modalidad de apoyo')
+    modalidad = forms.MultipleChoiceField(choices=MODALIDAD_CHOICE, label='Modalidad de apoyo')
     organizacion = forms.ModelMultipleChoiceField(queryset=Organizacion.objects.all(), label='Organizaciones')
     resultado = forms.ModelMultipleChoiceField(queryset=Resultado.objects.all(), label='Resultados')
     mes_inicio = forms.ChoiceField(choices=MONTH_CHOICES)
-    #periodo = forms.MultipleChoiceField(choices=CHOICE_PERIODO, label='Período')
-    #anio = forms.ChoiceField(choices=CHOICE_ANIO, label='Año')
+    anio_inicio = forms.ChoiceField(choices=ANIOS)
+    mes_fin = forms.ChoiceField(choices=MONTH_CHOICES)
+    anio_fin = forms.ChoiceField(choices=ANIOS)
--- fed/forms.py
+++ fed/forms.py
@@ -4,4 +4,4 @@ from django import forms
 ANIOS = ((0, 'Todos'), (2009, 2009), (2010, 2010), (2011, 2011))
 
 class GralForm(forms.Form):
-    year = forms.ChoiceField(choices=ANIOS, label=u'Año')
+    year = forms.ChoiceField(choices=ANIOS, label=u'Elegir Año')
