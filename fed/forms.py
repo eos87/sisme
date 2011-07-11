ee94bb871274f89757b75e9d40816ef616d66150
Helmy Giacoman <helmygb@gmail.com>
2011-07-11 16:21:00 -0600

    gantt graph almost :D

--- fed/forms.py
+++ fed/forms.py
@@ -1,7 +1,13 @@
 # -*- coding: UTF-8 -*-
 from django import forms
+from models import Organizacion
 
 ANIOS = ((0, 'Todos'), (2009, 2009), (2010, 2010), (2011, 2011))
+ANIO_2 = ((0, u'Año'), (2010, 2010), (2011, 2011))
 
 class GralForm(forms.Form):
     year = forms.ChoiceField(choices=ANIOS, label=u'Elegir Año')
+    
+class GanttForm(forms.Form):
+    anio = forms.ChoiceField(choices=ANIO_2, label=u'Elegir Año')
+    organizacion = forms.ModelChoiceField(queryset=Organizacion.objects.all(), empty_label='Organización')    
--- views.py
+++ views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from fed.forms import GanttForm
+from fed.models import Proyecto, Organizacion
+import datetime
 
 def index(request):
+    proys = {}
+    if request.method == 'POST':
+        form = GanttForm(request.POST)
+        if form.is_valid():
+            organizacion = form.cleaned_data['organizacion']
+            for proyecto in organizacion.proyecto_set.all():
+                proys[proyecto.codigo] = {mes: have_informe(proyecto, mes, form.cleaned_data['anio']) for mes in range(1,13)}                                 
+        pass
+    else:
+        form = GanttForm()
+        organizacion = Organizacion.objects.all().order_by('?')[0]
+        for proyecto in organizacion.proyecto_set.all():
+            proys[proyecto.codigo] = {mes: have_informe(proyecto, mes, datetime.date.today().year) for mes in range(1,13)}    
+    
     return render_to_response('index.html', RequestContext(request, locals()))
+
+
+def have_informe(proyecto, mes, year):
+    try:
+        inform = proyecto.informe_set.get(mes=mes, anio=year)
+        return True
+    except:
+        return False
