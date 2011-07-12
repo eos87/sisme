37c96392a6fcd3cc184c0c609bf783a764542221
Helmy Giacoman <helmygb@gmail.com>
2011-07-12 11:36:23 -0600

    gantt calendar finished XD

--- views.py
+++ views.py
@@ -5,14 +5,15 @@ from fed.models import Proyecto, Organizacion
 import datetime
 
 def index(request):
+    estilos = {1: 'month_estilo_1', 2: 'month_estilo_2', 3: 'month_estilo_3',
+               4: 'month_estilo_1', 5: 'month_estilo_2', 6: 'month_estilo_3'}
     proys = {}
     if request.method == 'POST':
         form = GanttForm(request.POST)
         if form.is_valid():
             organizacion = form.cleaned_data['organizacion']
             for proyecto in organizacion.proyecto_set.all():
-                proys[proyecto.codigo] = {mes: have_informe(proyecto, mes, form.cleaned_data['anio']) for mes in range(1,13)}                                 
-        pass
+                proys[proyecto.codigo] = {mes: have_informe(proyecto, mes, form.cleaned_data['anio']) for mes in range(1,13)}
     else:
         form = GanttForm()
         organizacion = Organizacion.objects.all().order_by('?')[0]
