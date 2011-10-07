779ad3624a009b593f1d31564e0c79f4db6e1fba
Helmy Giacoman <helmygb@gmail.com>
2011-10-07 15:50:46 -0600

    fix

--- static_media/files/js/graph.export.js
+++ static_media/files/js/graph.export.js
@@ -15,20 +15,3 @@ function exportGraph(data, title, elem){
 		}		
 	});	
 }
-
-(function($) {
-	var cache = [];
-	// Arguments are image paths relative to the current page.
-	$.preLoadImages = function() {
-		var args_len = arguments.length;
-		for ( var i = args_len; i--;) {
-			var cacheImage = document.createElement('img');
-			cacheImage.src = arguments[i];
-			cache.push(cacheImage);
-		}
-	}
-})(jQuery)
-
-$(document).ready(function(){
-	jQuery.preLoadImages("/files/img/spin.gif",);
-})
