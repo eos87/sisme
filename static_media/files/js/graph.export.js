d3a8b033f7fae0f762ab4786434f64adff665da1
Helmy Giacoman <helmygb@gmail.com>
2011-10-07 11:10:10 -0600

    spin listo :D

--- static_media/files/js/graph.export.js
+++ static_media/files/js/graph.export.js
@@ -1,4 +1,4 @@
-function exportGraph(data, title){
+function exportGraph(data, title, elem){
 	var foo = data;
 	var titulo = title;
 	var csrf_token =  $.cookie('csrftoken');	
@@ -7,18 +7,11 @@ function exportGraph(data, title){
 		title: titulo,
 		csrfmiddlewaretoken : csrf_token
 	}, function(data) {		
-		if(data!='error'){
-			var options = {
-				'width' : 970,
-				'height' : 500,
-				'autoScale' : false,
-				'type' : 'iframe'
-			}
-			var link = $('<a></a>').attr('href', '/view-graph/')
-			$(link).fancybox(options).trigger('click');
-			return;
+		if(data=='error'){
+			alert('Ha ocurrido un error :(');			
 		}else{
-			alert('Ha ocurrido un error :(');
-		}
+			$(elem).html('');					
+			$(link).fancybox(options).trigger('click');	
+		}		
 	});	
 }
